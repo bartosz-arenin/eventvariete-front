86b3bf8c19534d013bea9a6ab29f792914d93256
Bartosz-Arenin <bartosz.arenin@gmail.com>
2021-09-26 00:39:07 +0200

    More cfg heroku changes

--- server.js
+++ server.js
@@ -9,22 +9,12 @@ const express = require('express');
 const app = express();
 app.use(requireHTTPS);
 
-if (process.env.NODE_ENV === 'production'){
-    //set static folder
-    app.use(express.static('dist/eventvariete-front'));
-} else {
-    app.use(express.static('./dist/eventvariete-front'));
-}
+app.use(express.static('./dist/eventvariete-front'));
 
-// app.get('*', function(req, res) {
-//     res.sendFile('index.html', {root: '/dist/eventvariete-front/'}
-//   );
-//   });
-
-app.get('/*', function(req,res) {
-    
-    res.sendFile(path.join(__dirname+'/dist/eventvariete-front/index.html'));
-    });
+app.get('/*', function(req, res) {
+    res.sendFile('index.html', {root: './dist/eventvariete-front/'}
+  );
+  });
 
 app.listen(process.env.PORT || 8080);
 

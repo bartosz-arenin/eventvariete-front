4e8e3568511636ba65615f2abb33b17594ccdd42
Artur <artur.skoroda5@gmail.com>
2021-09-26 12:09:25 +0200

    Adding Dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,31 @@ import { HttpClient, HttpClientModule } from "@angular/common/http";
 import { EventListComponent } from './event-list/event-list.component';
 import { HomeComponent } from './home/home.component';
 import { AppHeaderComponent } from './home/app-header/app-header.component';
+import { ChartsModule } from 'ng2-charts';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavComponent } from './nav/nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+
+@NgModule({
+  imports: [
+   …
+   ChartsModule,
+   BrowserAnimationsModule,
+   LayoutModule,
+   MatToolbarModule,
+   MatButtonModule,
+   MatSidenavModule,
+   MatIconModule,
+   MatListModule,
+   …
+],
+  declarations: [NavComponent]
+})
 
 @NgModule({
   declarations: [
